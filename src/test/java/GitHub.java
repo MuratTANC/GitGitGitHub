5ddcdff6ccc3719cc95c45a750831c21857b4bfc
murattanc@gmail.com <Yusuf34+>
2022-03-30 22:52:08 +0300

    1.2

--- src/test/java/GitHub.java
+++ src/test/java/GitHub.java
@@ -2,5 +2,6 @@ public class GitHub {
     public static void main(String[] args) {
         System.out.println("Git Hub");
         System.out.println("1. değişiklik");
+        System.out.println("2. değişiklik");
     }
 }
